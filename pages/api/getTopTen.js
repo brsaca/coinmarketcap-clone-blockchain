f2a49b5ef67b8ddc4d7b9ed3d6575bd85a1a5b02
Brenda SC <brenda.saavedra@fondeadora.com>
2022-08-22 17:43:37 -0500

    fix: fetch to sandbox

--- pages/api/getTopTen.js
+++ pages/api/getTopTen.js
@@ -1,15 +1,18 @@
 export default function handler(req, res){
     const getData = async () => {
-        const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${process.env.CMC_API_KEY}',
+      
+       const response = await fetch('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' ,
             {
                 method: 'GET',
                 headers: {
+                    'X-CMC_PRO_API_KEY': '${process.env.CMC_API_KEY}',
                     Accept: '*/*'
                 },
             },
         )
 
         const data = await response.json()
+        console.log(data)
 
         res.status(200).json({data})
     }
