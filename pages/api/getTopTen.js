b740ff55dd50fa5da15101efcae1fb84ed00e7b7
Brenda SC <brenda.saavedra@fondeadora.com>
2022-08-22 18:03:43 -0500

    fix: correct forma url

--- pages/api/getTopTen.js
+++ pages/api/getTopTen.js
@@ -1,11 +1,10 @@
 export default function handler(req, res){
     const getData = async () => {
-      
-       const response = await fetch('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' ,
+       const response = await fetch(
+        `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${process.env.CMC_API_KEY}`,
             {
                 method: 'GET',
                 headers: {
-                    'X-CMC_PRO_API_KEY': '${process.env.CMC_API_KEY}',
                     Accept: '*/*'
                 },
             },
