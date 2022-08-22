57856bee463f76821560fa82c20921cde9d71a47
Brenda SC <brenda.saavedra@fondeadora.com>
2022-08-22 18:58:27 -0500

    style: add format to header

--- components/cmc-table/CMCTable.js
+++ components/cmc-table/CMCTable.js
@@ -2,6 +2,7 @@ import React, {useState, useContext, useEffect, useCallback} from 'react'
 import btc from '../../assets/btc.png'
 import { CoinMarketContext } from '../../context/context'
 import CMCTableHeader from '../cmc-table/CMCTableHeader'
+import CMCTableRow from '../cmc-table/CMCTableRow'
 
 const CMCTable = () => {
     let { getTopTenCoins } = useContext(CoinMarketContext);
@@ -16,11 +17,13 @@ const CMCTable = () => {
             let apiResponse = await getTopTenCoins()
             let filteredResponse = []
 
-            for(let i = 0; i < apiResponse.length; i++){
-                const element = apiResponse[i]
-                if(element.cmc_rank <= 10) filteredResponse.push(element)
-            }
+            console.log(apiResponse.length)
 
+            for (let i = 0; i < apiResponse.length; i++) {
+                const element = apiResponse[i]
+                if (element.cmc_rank <= 10) filteredResponse.push(element)
+            }
+            console.log(filteredResponse)
             setCoinData(filteredResponse)
         }catch(e) {
             console.log(e.message);
@@ -34,26 +37,28 @@ const CMCTable = () => {
                     <CMCTableHeader /> 
                     {coinData && coinData ? (
                         coinData.map((coin, index) => {
-                            return (
-                                <CMCTableRow
-                                    key = {index}
-                                    starNum = {coin.cmc_rank}
-                                    coinName = {coin.name}
-                                    coinSymbol = {coin.symbol}
-                                    coinIcon = {btc}
-                                    showBuy = {true}
-                                    hRate = {coin.quote.USD.percent_change_24H}
-                                    dRate = {coin.quote.USD.percent_change_7D}
-                                    hRateIsIncrement = {true}
-                                    price = {coin.quote.USD.price}
-                                    marketCapValue = {coin.quote.USD.market_cap}
-                                    volumeCryptoValue = {coin.quote.USD.volume_24H}
-                                    volumeValue = {coin.total_supply}
-                                    circulatingSupply = {coin.circulating_supply}
-                                />
-                            )
+                        return (
+                            <CMCTableRow
+                            key={index}
+                            starNum={coin.cmc_rank}
+                            coinName={coin.name}
+                            coinSymbol={coin.symbol}
+                            coinIcon={btc}
+                            showBuy={true}
+                            hRate={coin.quote.USD.percent_change_24h}
+                            dRate={coin.quote.USD.percent_change_7d}
+                            hRateIsIncrement={true}
+                            price={coin.quote.USD.price}
+                            marketCapValue={coin.quote.USD.market_cap}
+                            volumeCryptoValue={coin.quote.USD.volume_24h}
+                            volumeValue={coin.total_supply}
+                            circulatingSupply={coin.circulating_supply}
+                            />
+                        )
                         })
-                    ) : <></>}
+                    ) : (
+                        <></>
+                    )}
                 </table>
             </div>
         </div>
--- components/cmc-table/CMCTableHeader.js
+++ components/cmc-table/CMCTableHeader.js
@@ -3,7 +3,8 @@ import ChevronDown from '../../assets/svg/chevronDown'
 import Info from '../../assets/svg/info'
 
 const styles = {
-    textIcon: 'flex items-center'
+    textIcon: 'flex items-center',
+    title: 'mr-2'
 }
 
 class CMCTableHeader extends Component {
@@ -13,6 +14,14 @@ class CMCTableHeader extends Component {
                 <tr>
                     <th></th>
                     <th className={styles.textIcon}><b># &nbsp;</b><ChevronDown/></th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>24h %</th>
+                    <th>7d %</th>
+                    <th><div className={styles.textIcon}><p className={styles.title}>Market Cap</p><Info/></div></th>
+                    <th><div className={styles.textIcon}><p className={styles.title}>Volume</p><Info/></div></th>
+                    <th><div className={styles.textIcon}><p className={styles.title}>Circulating Supply</p><Info/></div></th>
+                    <th>Last 7 days</th>
                 </tr>
                 
             </tbody>
