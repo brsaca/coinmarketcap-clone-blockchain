a08d5434490dabf1170293b938b3eae9a45a3b54
Brenda SC <brenda.saavedra@fondeadora.com>
2022-08-25 11:39:10 -0500

    feat: add contracts for mint tokens

---
+++ smartcontracts/contracts/Dogecoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DogeCoin is ERC20, Ownable {
+    constructor() ERC20("DogeCoin", "DOGE") {
+    }
+
+    function mint(address to, uint256 amount) public  payable{
+        _mint(to, amount);
+    }
+
+    // accept ETH, do nothing as it would break the gas fee for a transaction
+    receive() external payable {}
+}
---
+++ smartcontracts/contracts/Link.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Link is ERC20, Ownable {
+    constructor() ERC20("Link", "LINK") {
+    }
+
+    function mint(address to, uint256 amount) public  payable{
+        _mint(to, amount);
+    }
+
+    // accept ETH, do nothing as it would break the gas fee for a transaction
+    receive() external payable {}
+}
